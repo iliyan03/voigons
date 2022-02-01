98851367d3af1f46c18a17bcc8d6c49c40a1dd98
iliyan03 <80851055+iliyan03@users.noreply.github.com>
2022-02-01 16:16:03 +0200

    firebase project changed to voigon

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-analytics.js";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCfmdBMXdyTxwtXHng4kdRVJNMg9Zs2p38",
-    authDomain: "anayltics-test-e6628.firebaseapp.com",
-    projectId: "anayltics-test-e6628",
-    storageBucket: "anayltics-test-e6628.appspot.com",
-    messagingSenderId: "312588800976",
-    appId: "1:312588800976:web:901d931bf0aa1284699fd5",
-    measurementId: "G-7Q8D2X7WK4"
+  apiKey: "AIzaSyBqFSVKAcnreBvxgXxqIQA6hDWAmP5cYJY",
+  authDomain: "voigon.firebaseapp.com",
+  databaseURL: "https://voigon.firebaseio.com",
+  projectId: "voigon",
+  storageBucket: "voigon.appspot.com",
+  messagingSenderId: "1050910999548",
+  appId: "1:1050910999548:web:f8780cc43748e81952e879",
+  measurementId: "G-8724GMTP9W"
 };
 
 // Initialize Firebase
