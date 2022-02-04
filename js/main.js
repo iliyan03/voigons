ec500edccf1a6aae7a17c7d5f5633c7a86e20e52
iliyan03 <iliyankovachev61@gmail.com>
2022-02-04 16:56:03 +0200

    tasks done

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-function showBar(){
-    let responsiveButtons = document.getElementById("responsiveButtons");
-    if (responsiveButtons.classList.contains("active")){
-        responsiveButtons.classList.remove("active")
-    } else{
-        responsiveButtons.classList.add("active")
-    }
+/* Change favicon based on OS theme */
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  var link = document.querySelector("link[rel~='icon']");
+  link.href = "https://iliyan03.github.io/voigons/images/logo.png";
 }
 
+/* Show navbar options on mobile devices */
+function showBar(){
+  let responsiveButtons = document.getElementById("responsiveButtons");
+  if (responsiveButtons.classList.contains("active")){
+      responsiveButtons.classList.remove("active")
+  } else{
+      responsiveButtons.classList.add("active")
+  }
+}
+
+/* Slideshow */
 var slideIndex = 1;
 let timer;
 showSlides(slideIndex);
@@ -20,8 +28,8 @@ function currentSlide(n) {
 }
 
 function startTimer(){
-  timer = setInterval(plusSlides, 5000, 1)
-}
+/*   timer = setInterval(plusSlides, 5000, 1)
+ */}
 
 function showSlides(n) {
   var i;
@@ -40,5 +48,55 @@ function showSlides(n) {
   if (timer){
     clearInterval(timer);
   }
-  startTimer(slideIndex)
+  startTimer()
 }
+/* Slideshow ends here */
+
+
+/* Youtube API for slideshow iframes */
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[2];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+var player_2;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('blasting_courier_player', {
+    videoId: 'mWy210QVbNU',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+  player_2 = new YT.Player('premortal', {
+    videoId: 'Y_8-oX6VCjA',
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+let done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING) {
+    clearInterval(timer);
+  } else if (event.data == YT.PlayerState.PAUSED){
+    startTimer()
+  }
+}
+
+/* Youtube API for slideshow iframes - ends here */
