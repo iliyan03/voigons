497dd0c9d4759db9d03ca96193cc32efefb6184a
iliyan03 <iliyankovachev61@gmail.com>
2022-02-04 13:58:32 +0200

    image upload

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ function showBar(){
 }
 
 var slideIndex = 1;
+let timer;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -18,11 +19,15 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+function startTimer(){
+  timer = setInterval(plusSlides, 5000, 1)
+}
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
@@ -32,4 +37,8 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-}
+  if (timer){
+    clearInterval(timer);
+  }
+/*   startTimer(slideIndex)
+ */}
